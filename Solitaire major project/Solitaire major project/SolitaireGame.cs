f503d04df7306edb7bf1a049c86c15f14f7c795d
Liam-Bolan <144348048+Liam-Bolan@users.noreply.github.com>
2024-07-01 11:23:41 +0100

    Add files via upload

---
+++ Solitaire major project/Solitaire major project/SolitaireGame.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Solitaire_major_project
+{
+    public partial class SolitaireGame : Form
+    {
+        List<Card> CardsOnScreen = new List<Card>();
+        public SolitaireGame()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void SolitaireGame_Load(object sender, EventArgs e)
+        {
+            
+            deck deck = new deck();
+            deck.Shuffle();
+            for (int i = 24; i > 0; i--)
+            {
+                Card c = deck.Draw();
+                c.Location = new Point(1*i, 53);
+                c.Parent = this;
+
+            }
+            for (int i = 7; i > 0; i--)
+            {
+                Card c = deck.Draw();
+
+                c.Location = new Point(1250, 500 + 10 * i);
+                c.Parent = this;
+
+            }
+            for(int i = 6; i > 0; i--)
+            {
+                Card c = deck.Draw();
+
+                c.Location = new Point(1050, 500 + 10 * i);
+                c.Parent = this;
+            }
+            for (int i = 5; i > 0; i--)
+            {
+                Card c = deck.Draw();
+
+                c.Location = new Point(850, 500 + 10 * i);
+                c.Parent = this;
+            }
+            for (int i = 4; i > 0; i--)
+            {
+                Card c = deck.Draw();
+
+                c.Location = new Point(650, 500 + 10 * i);
+                c.Parent = this;
+            }
+            for (int i = 3; i > 0; i--)
+            {
+                Card c = deck.Draw();
+
+                c.Location = new Point(450, 500 + 10 * i);
+                c.Parent = this;
+            }
+            for (int i = 2; i > 0; i--)
+            {
+                Card c = deck.Draw();
+
+                c.Location = new Point(250, 500 + 10 * i);
+                c.Parent = this;
+            }
+            for (int i = 1; i > 0; i--)
+            {
+                Card c = deck.Draw();
+
+                c.Location = new Point(50, 500 + 10 * i);
+                c.Parent = this;
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Solitaire major project/Solitaire major project/projectclasses.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Markup;
+
+namespace Solitaire_major_project
+{
+    public enum suits
+    {
+        hearts, diamonds,spades,clubs
+    }
+    class Card : PictureBox
+    {
+        private string[] names = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+        private string[] suits = { "hearts", "diamonds", "spades", "clubs" };
+        public int value { get; set; }
+        public suits suit { get; set; }
+
+        public bool faceup { get; set; }
+
+        public string name()
+        {
+            return names[value - 1];
+        }
+
+        public Card(int v ,int s) : base()
+        {
+            value = v;
+            suit = (suits)s;
+            faceup = true;
+
+            string imagefile="";
+            if (faceup)
+            {
+
+                imagefile = "../../resources/" + names[value - 1] + "_of_" + suits[(int)suit] + ".png";
+            }
+            else
+            {
+
+            }
+            Image = Image.FromFile(imagefile);
+            MouseMove += new MouseEventHandler(Card_MouseDown);
+            SizeMode = PictureBoxSizeMode.StretchImage;
+            Margin = new Padding(0, 0, 0, 0);
+            Width = 150;
+            Height = 220;
+        }
+
+        
+        private void Card_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Left += e.X - 80;
+                Top += e.Y - 110;
+
+            }
+
+        }
+
+    }
+
+    class deck
+    {
+        private Card[] cards = new Card[52];
+        private int pos = 0;
+
+        public deck()
+        {
+            for (int s = 0; s < 4; s++)
+            {
+                for (int v = 0; v < 13; v++)
+                {
+                    Card c = new Card(v+1,s);
+                    cards[s * 13 + v] = c;
+                }
+            }
+        }
+
+        public Card Draw()
+        {
+            pos++;
+            return cards[pos - 1];
+        }
+        public void Shuffle()
+        {
+            Random r = new Random();
+            for (int i = 0; i < 200; i++)
+            {
+               
+                int ran = r.Next(0, 52);
+                int ran2 = r.Next(0, 52);
+                Card c1 = cards[ran];
+                cards[ran] = cards[ran2];
+                cards[ran2] = c1;
+                
+            }
+       
+        }
+        
+    }
+
+}
